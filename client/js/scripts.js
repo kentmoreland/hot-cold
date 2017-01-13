b34b994f69bf1414eaa94dff71db3bb2fc60f0cc
Kent <kentmoreland@gmail.com>
2017-01-12 23:22:58 -0600

    update: added docs to gitignore

--- client/js/scripts.js
+++ client/js/scripts.js
@@ -3,8 +3,11 @@ $(document).ready(() => {
   $('#player_guess').click((event) => {
     event.preventDefault();
     let num = Math.floor((Math.random() * 10) + 1);
+    // let num = 5;
     let guess = $('#guess').val();
     console.log('player guess: ', guess, 'rando num: ', num )
+    guess === num.toString() ? $('#message').text('YOU WIN') : $('#message').text('Oh well... maybe next time');
+
     $('#guess').val('')
   });
 
