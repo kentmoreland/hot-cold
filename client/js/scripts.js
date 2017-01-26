99400d488631d5cd915541b28e22845245a97518
Kent <kentmoreland@gmail.com>
2017-01-26 00:12:11 -0600

    refactor: modularized functions in scripts.js

--- client/js/scripts.js
+++ client/js/scripts.js
@@ -11,15 +11,33 @@ $(document).ready(() => {
     return Math.floor(Math.random() * num) + 1;
   };
 
-  let checkFirstGuess = (guess, target) => {
-    if(guess === target){ return 'Winner Winner Chicken Dinner'; }
+  let getGuess = (idName) => {
+    return parseInt($(idName).val());
+  }
+
+  let checkValidGuess = (guess, guessMax) => {
+   return  guess > 0 && guess < guessMax ? true : false;
+  };
+
+  let addGuess = (isValidGuess, guess, guessArray) => {
+    if(isValidGuess){ guessArray.push(guess) };
+  };
+
+  let showGuess = (isValidGuess, idName, guess) => {
+    if(isValidGuess){ $(idName).append(guess + ' ') }
+  }
+
+  let checkFirstGuess = (guess, target, guessRange) => {
+    if(guess > guessRange){return `Guess must be less than ${guessRange}`}
+    else if(guess === target){ return 'Winner Winner Chicken Dinner'; }
     else if(Math.abs(guess - target) <= 5){ return 'Hot'; }
     else if(Math.abs(guess - target) <= 10){ return 'Warm'; }
     else if(Math.abs(guess - target) <= 15){ return 'Cool'; }
     else { return 'Cold'; }
   };
 
-  let checkNextGuess = (prevGuess, currGuess, target) => {
+  let checkNextGuess = (prevGuess, currGuess, target, guessRange) => {
+    if(currGuess > guessRange){return `Guess must be less than ${guessRange}`}
     if(currGuess === target){ return 'Winner Winner Chicken Dinner'; }
 
     let prevGuessDist = Math.abs(prevGuess - target);
@@ -29,19 +47,23 @@ $(document).ready(() => {
     if(currGuessDist < prevGuessDist){ return 'your getting warmer'; }
     else if(currGuessDist > prevGuessDist){ return 'your getting colder'; }
     else{ return 'you didn\'t gain any ground with that guess'; }
-};
+  };
 
 
   let playGuess = (e) =>{
     e.preventDefault();
-    guess = $('#guess').val();
-    guess = parseInt(guess);
-    guesses.push(guess);
+
+    guess = getGuess('#guess');
+
+    let isValidGuess = checkValidGuess(guess, guessRange);
+
+    addGuess(isValidGuess, guess, guesses);
+
 
     if(firstGuess === true){
       firstGuess = false;
       target = getRandom(guessRange);
-      message = checkFirstGuess(guess, target);
+      message = checkFirstGuess(guess, target, guessRange);
 
     } else {
 
@@ -49,7 +71,7 @@ $(document).ready(() => {
       message = checkNextGuess(previousGuess, guess, target);
     }
 
-    $('#guess_list').append(guess + ' ');
+    showGuess(isValidGuess, '#guess_list', guess )
     $('#message').text(message);
     $('#guess').val('');
 
